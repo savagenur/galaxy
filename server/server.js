609954eefdf05d0905b8b2b45319d736756adcaf
Nurbolot Muratbekuulu <savagenurkg@gmail.com>
2025-08-20 01:19:51 -0700

    Fix API route in server.js from "/app/inngest" to "/api/inngest"

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Server is running"));
-app.use("/app/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
