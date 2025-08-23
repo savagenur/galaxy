ebd0c124643db39e0fc488edaa360863a1214f84
Nurbolot Muratbekuulu <savagenurkg@gmail.com>
2025-08-22 18:38:45 -0700

    Add user authentication and profile management features with image upload support

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { functions, inngest } from "./inngest/index.js";
 import { serve } from "inngest/express";
-
+import { clerkMiddleware } from '@clerk/express'
 const app = express();
 await connectDB();
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
